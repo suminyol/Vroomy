dd05391aab42d84f3b860355fcb42354ecddcb69
Suminder <sumi786nyol@gmail.com>
2025-07-14 20:01:18 +0530

    fix: update background gradient colors in Banner component for improved visual consistency

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -9,7 +9,7 @@ function Banner() {
             initial={{opacity:0,y:50}}
             whileInView={{opacity:1,y:0}}
             transition={{duration:0.6}} className='flex flex-col md:flex-row md:items-start items-center
-        justify-between px-8 min-md:pl-14 pt-10 bg-gradient-to-r from-[#0558FE] to-[#A9CFFF] max-w-6xl mx-3 md:mx-auto rounded-2xl overflow-hidden'>
+        justify-between px-8 min-md:pl-14 pt-10 bg-gradient-to-r from-[#ff416c] to-[ #ff4b2b] max-w-6xl mx-3 md:mx-auto rounded-2xl overflow-hidden'>
                     <div className='text-white'> 
                         <h2 className='text-3xl font-medium'>Do You Own a Car?</h2> 
                         <p className='mt-2'>Monetize your vehicle effortlessly by listing it on CarRental.</p>
